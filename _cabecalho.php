c4de6e0407f7c85b9b645b46249e53488958118d
OzielMachado <oziel-bmachado@educar.rs.gov.br>
2022-10-27 09:46:20 -0300

    estrutura base da aplicação

---
+++ _cabecalho.php
@@ -0,0 +1,20 @@
+<?php 
+    // ========================================================
+    // cabeçalho
+    // ========================================================
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ANTIRRANSOWRE</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="inc/main.css">
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+    // ========================================================
+    // index
+    // ========================================================
+
+
+    include_once('_cabecalho.php');
+
+    echo '<p>teste de fontawesome <span class="fa fa-times"></span></p>';
+
+    include_once('_rodape.php');
+
+?>
